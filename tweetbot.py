5b4876659c2b59c2b436338777d7bec635354816
PabloCanovas <pablocanovastourne@gmail.com>
2023-04-03 19:24:35 +0200

    fix counter

--- tweetbot.py
+++ tweetbot.py
@@ -2,9 +2,8 @@ import os
 import tweepy
 from utils.file_utils import *
 
-path = "tweets"
-count_file = f'{path}/tweet_count.txt'
-tweets_file = f'{path}/trading_tweets.txt'
+counter_file = 'utils/tweet_counter.txt'
+tweets_file = 'tweets/trading_tweets.txt'
 
 def main():
     client = tweepy.Client(consumer_key = os.environ.get('CONSUMER_KEY'), 
@@ -12,7 +11,7 @@ def main():
                            access_token = os.environ.get('ACCESS_TOKEN'), 
                            access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET'))
 
-    count = get_tweet_count(count_file)
+    count = get_tweet_count(counter_file)
     tweet = get_tweet(tweets_file, count)
     print(tweet)
 
